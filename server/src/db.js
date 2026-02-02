cdc35deb1eef8a010420c7bfe1fe7f473dd491c7
UmidYul <yumid253@gmail.com>
2026-02-02 11:33:37 +0500

    db

--- server/src/db.js
+++ server/src/db.js
@@ -4,7 +4,7 @@ const { Pool } = pkg;
 const pool = new Pool({
     host: process.env.PGHOST || '127.0.0.200',
     port: process.env.PGPORT ? parseInt(process.env.PGPORT) : 5432,
-    database: process.env.PGDATABASE || 'zedlyuz_testDB',
+    database: process.env.PGDATABASE || 'zedlyuz_DB',
     user: process.env.PGUSER || 'zedlyuz_umid',
     password: process.env.PGPASSWORD || 'g@laxyA7',
 });
